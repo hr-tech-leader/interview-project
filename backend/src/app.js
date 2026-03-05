75ab070871d0fab4613c256ae0b31550a53b7ef9
hr-tech-reader <jaimeandujo086@gmail.com>
2026-03-04 21:39:43 -0800

    updateProject

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,6 +17,6 @@ app.use('/api/categories', categoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
